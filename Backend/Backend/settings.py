ecfc99aa97c1f3bc5c368b99c5f196b2bf6b4252
Danmamaz <danilolitvin904@gmail.com>
2025-11-21 15:12:19 +0200

    Fixing bugs

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -24,13 +24,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-<<<<<<< HEAD
-SECRET_KEY = 'django-insecure-super-secret-key-for-dev-12345'
-
-=======
 SECRET_KEY = os.getenv("SECRET_KEY")
->>>>>>> ee72244c6532dfb87ea48ded4361adfd1176a23f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -51,7 +45,6 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "Auth",
     "Courses",
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
